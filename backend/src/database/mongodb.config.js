7d14590637f3c792f1d4c1e8cdeb38ec6b96b439
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-18 07:13:08 +0530

    Establish MongoDB connection

---
+++ backend/src/database/mongodb.config.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const establishDBConnection = async () => {
+  try {
+    const mongoURI = `${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`;
+    await mongoose.connect(mongoURI);
+    console.log(`MongoDB Connection Established
+        MongoDBHost : ${process.env.MONGO_DB_URI}
+        Database : ${process.env.DB_NAME}`);
+    return true;
+  } catch (error) {
+    console.error(`MongoDB Connection Failed!
+      ${error.name}
+      ${error.message}\n`);
+    return false;
+  }
+};
+
+export default establishDBConnection;
