a15eef9958a8be3ed604ebbea808cfc8c497e62d
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-20 01:15:07 +0530

    update database configurations

--- backend/src/database/mongodb.config.js
+++ backend/src/database/mongodb.config.js
@@ -1,18 +1,33 @@
+// backend/src/database/mongodb.config.js
+
 import mongoose from "mongoose";
 
+// Function to establish connection with MongoDB database
 const establishDBConnection = async () => {
   try {
-    const mongoURI = `${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`;
-    await mongoose.connect(mongoURI);
-    console.log(`MongoDB Connection Established
-        MongoDBHost : ${process.env.MONGO_DB_URI}
-        Database : ${process.env.DB_NAME}`);
-    return true;
+    // Construct MongoDB URI using environment variables
+    const { MONGODB_URI, DB_NAME, MONGODB_URI_OPTIONS } = process.env;
+
+    // Validate required environment variables
+    if (!MONGODB_URI || !DB_NAME || !MONGODB_URI_OPTIONS) {
+      throw new Error("Missing MONGODB_URI or DB_NAME or MONGODB_URI_OPTIONS in environment variables.");
+    }
+    // Attempt to connect to MongoDB
+    const mongoDBconnection = await mongoose.connect(MONGODB_URI + DB_NAME + MONGODB_URI_OPTIONS);
+
+    // Log successful connection details
+    console.info(`\nMongoDB Connected Successfully:
+      Host Name : ${mongoDBconnection.connection.host}
+      Database Name : ${mongoDBconnection.connection.name}
+      Connection String : ${mongoDBconnection.connection._connectionString}`);
+
+    return true; // Return true if connection is successful
   } catch (error) {
-    console.error(`MongoDB Connection Failed!
-      ${error.name}
-      ${error.message}\n`);
-    return false;
+    // Log error details if connection fails
+    console.error(`\nMongoDB Connection Failed!
+            ${error.name}
+            ${error.message}\n`);
+    return false; // Return false if connection fails
   }
 };
 
