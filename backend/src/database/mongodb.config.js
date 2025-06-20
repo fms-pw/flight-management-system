28e690c89daf746a2ad8d2d7a9d6caebd0440758
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-20 19:28:16 +0530

    update database configurations

--- backend/src/database/mongodb.config.js
+++ backend/src/database/mongodb.config.js
@@ -1,13 +1,20 @@
-// backend/src/database/mongodb.config.js
-
 import mongoose from "mongoose";
+
+// Construct MongoDB URI using environment variables
+let { MONGODB_URI, DB_NAME, MONGODB_URI_OPTIONS, MONGODB_URI_LOCAL, NODE_ENV } = process.env;
+
+// Get the current environment (development or production)
+const NodeEnv = NODE_ENV;
+console.info(`\nEnvironment Mode : ${NodeEnv}`);
+
+// In development mode, override and use local database
+if (NodeEnv === "development") {
+  MONGODB_URI = MONGODB_URI_LOCAL;
+}
 
 // Function to establish connection with MongoDB database
 const establishDBConnection = async () => {
   try {
-    // Construct MongoDB URI using environment variables
-    const { MONGODB_URI, DB_NAME, MONGODB_URI_OPTIONS } = process.env;
-
     // Validate required environment variables
     if (!MONGODB_URI || !DB_NAME || !MONGODB_URI_OPTIONS) {
       throw new Error("Missing MONGODB_URI or DB_NAME or MONGODB_URI_OPTIONS in environment variables.");
