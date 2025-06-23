5190e0cc6460f5a032df3e22ceecb3ace6ffd2ec
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-24 01:17:50 +0530

    add more api endpoints and update existing routes

--- backend/src/routers/auth.routes.js
+++ backend/src/routers/auth.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controllers.js";
+import { forgotUserPassword, loginUser, logoutUser, signupUser } from "../controllers/auth.controllers.js";
 
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+router.post("/signup", signupUser);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/forgot-password", forgotUserPassword);
 
 export default router;
