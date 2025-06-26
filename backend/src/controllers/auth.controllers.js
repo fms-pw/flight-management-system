79f4d3508aea6b833a84b0f04e6a54ad45cc7648
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-27 05:26:53 +0530

    update https response body messages in route handler

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -20,7 +20,7 @@ export const signupUser = async (req, res) => {
 
       // Respond with specific duplicate field message
       return res.status(400).json({
-        status: `failed`,
+        success: false,
         message: `An account already exists with this ${duplicates}`,
       });
     }
@@ -28,7 +28,7 @@ export const signupUser = async (req, res) => {
     // Create a new user with the data from the request body
     const newUser = await User.create(req.body);
 
-    // Data to encode in the token (payload)
+    // Data to encode in the token
     const payload = {
       userId: newUser._id.toString(),
       email: newUser.email,
@@ -36,24 +36,24 @@ export const signupUser = async (req, res) => {
     };
 
     // Log the payload for debugging purposes
-    console.debug("\nPayload for Token Generation ", payload);
+    console.debug("\nPayload for Token Generation \n", payload);
 
     // Generate JWT Token
     const token = await generateJWTtoken(payload, res);
 
     // Log the generated token for debugging purposes
-    console.debug("\nGenerated JWT Token at Signup: ", token);
+    console.debug("\nGenerated JWT Token at Signup \n", token);
 
     res.status(201).json({
-      status: `success`,
+      success: true,
       message: `Congratulation! ${newUser.firstName} ${newUser.lastName} Your account has been created successfully`,
-      UserId: newUser._id,
     });
   } catch (error) {
     res.status(500).json({
-      status: "User registration failed",
+      success: false,
+      message: "User registration failed",
       error: error.name,
-      message: error.message,
+      details: error.message,
     });
   }
 };
@@ -66,13 +66,14 @@ export const loginUser = async (req, res) => {
     // Allow only one of email or mobileNumber, not both or none
     if ((!email && !mobileNumber) || (email && mobileNumber)) {
       return res.status(400).json({
+        success: false,
         message: "Please provide either email or mobile number, not both.",
       });
     }
 
     // Password is required for login
     if (!password) {
-      return res.status(400).json({ message: "Password is required." });
+      return res.status(400).json({ success: false, message: "Password is required." });
     }
 
     // Find the user by email or mobile number, include password field
@@ -82,7 +83,7 @@ export const loginUser = async (req, res) => {
 
     // If user not found, send error
     if (!user) {
-      return res.status(401).json({ message: "User not found!" });
+      return res.status(401).json({ success: false, message: "User not found!" });
     }
 
     // Retrieve the stored hashed password from the user document
@@ -93,7 +94,7 @@ export const loginUser = async (req, res) => {
 
     // If password does not match, send error
     if (!isPasswordMatch) {
-      return res.status(401).json({ message: "Invalid Password" });
+      return res.status(401).json({ success: false, message: "Invalid Password" });
     }
 
     // Data to encode in the token (payload)
@@ -106,17 +107,18 @@ export const loginUser = async (req, res) => {
     const token = await generateJWTtoken(payload, res);
 
     // Log the generated token for debugging purposes
-    console.debug("\nGenerated JWT Token at Login: ", token);
+    console.debug("\nGenerated JWT Token at Login \n", token);
 
     res.status(200).json({
-      status: "success",
+      success: true,
       message: `Welcome back, ${user.firstName}! You have logged in successfully.`,
     });
   } catch (error) {
     res.status(500).json({
-      status: "Something went wrong",
+      success: false,
+      message: "User login failed",
       error: error.name,
-      message: error.message,
+      details: error.message,
     });
   }
 };
