04cdab219f4f5ff023c474103c9975b63a1061c2
Kailash Parihar <kailashparihar1999@gmail.com>
2025-06-27 05:19:40 +0530

    add 404 Route Not Found Handler and add /ping Route for test auth middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,30 +2,52 @@ import express from "express";
 import cookieParser from "cookie-parser";
 
 import { establishDBConnection } from "./database/mongodb.config.js";
+
 import authRoutes from "./routers/auth.routes.js";
 import userRoutes from "./routers/user.routes.js";
 
+import authenticateUser from "./middlewares/auth.middleware.js";
+
 const server = express();
 
+// Parse incoming JSON requests and cookies with specified limits
 server.use(express.json({ limit: "16kb" }));
 server.use(cookieParser());
 
+// Test route to verify authentication middleware
+server.get("/ping", authenticateUser, (req, res) => {
+  return res.status(200).json({
+    success: true,
+    message: `Ping Pong Ping ! ${req.user?.email} You are Authenticated and working fine.`,
+  });
+});
+
+// Register authentication and user-related API routes
 server.use("/api/v1/auth", authRoutes);
 server.use("/api/v1/users", userRoutes);
 
+// Handle undefined routes with a 404 Not Found response
+server.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+// Function to start the server after establishing a database connection
 const startServer = async () => {
   try {
-    // Establish a connection to the database
+    // Attempt to connect to the database before starting the server
     const isDBConnected = await establishDBConnection();
 
     if (!isDBConnected) {
       console.error("\nDatabase connection failed. Aborting server startup\n");
-      process.exit(1); // Exit the process if the database connection fails
+      process.exit(1); // Exit if DB connection fails
     }
 
     const port = process.env.PORT || 7000;
 
-    // Start the server and listen on the specified port
+    // Start listening for incoming requests on the specified port
     server.listen(port, () => {
       console.info(`\nServer is start running at http://localhost:${port}\n`);
     });
@@ -35,4 +57,5 @@ const startServer = async () => {
     process.exit(1); // Exit the process with an error code
   }
 };
+
 export default startServer;
