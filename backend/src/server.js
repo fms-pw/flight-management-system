e76921c694850def327ad44b077f42e98e158a62
Kailash Parihar <kailashparihar1999@gmail.com>
2025-07-09 04:58:40 +0530

    Added global error handling middleware to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,47 +8,49 @@ import userRoutes from "./routers/user.routes.js";
 
 import { flightsRouter } from "./routers/flights.routes.js";
 import { fli_compRouters } from "./routers/fli_comp.routes.js";
+import { fli_statusRouter } from "./routers/flights_status.routes.js";
 
+// Import custom middleware for authentication, authorization, and error handling
 import authenticateUser from "./middlewares/auth.middleware.js";
 import authorizeUserRoles from "./middlewares/authorizeRole.middleware.js";
-import { fli_statusRouter } from "./routers/flights_status.routes.js";
+import errorHandler from "./middlewares/errorHandler.middleware.js";
 
 const server = express();
 
+// Parsing incoming JSON requests and cookies
 server.use(express.json({ limit: "16kb" }));
 server.use(cookieParser());
 
-server.get("/", (req, res) => {
-  return res.status(200).json({ message: "Flight Management System" });
-});
+server.get("/", (req, res) => res.status(200).json({ status: "success", message: "OK" }));
 
-// Test authenticate middleware using ping route
+// Protected route to test authentication and authorization middleware
 server.get("/protected", authenticateUser, authorizeUserRoles(["admin", "manager"]), (req, res) => {
   return res.status(200).json({
     success: true,
     message: `Ping Pong Ping ! You are Authenticated and Authorised ${req.user?.email} and working fine.`,
   });
 });
-//routes
+
+// Registering API routes
 server.use("/api/v1/auth", authRoutes);
 server.use("/api/v1/users", userRoutes);
+
 server.use("/api/v1/flights", flightsRouter);
 server.use("/api/v1/fli_comp", fli_compRouters);
-
 server.use("/api/v1/fli_status", fli_statusRouter);
 
-// Handling undefined routes with a 404 Not Found response
+// Handle undefined routes with a 404 response
 server.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "404 Route Not Found",
-  });
+  res.status(404).json({ success: false, message: "404 Route Not Found" });
 });
 
-// Function to start the server after a successful database connection
+// Global error handler middleware
+server.use(errorHandler);
+
+// Function to start the server after establishing a database connection
 const startServer = async () => {
   try {
-    // Connect to the database before starting the server
+    // Connecting to the database before starting the server
     const isDBConnected = await establishDBConnection();
 
     if (!isDBConnected) {
