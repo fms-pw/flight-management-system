562128e2097b85aba0541a1706f3c33d8341512c
Kailash Parihar <kailashparihar1999@gmail.com>
2025-07-09 05:24:52 +0530

    Added getMyProfile route endpoint, and filtered public and protected routes

--- backend/src/routers/auth.routes.js
+++ backend/src/routers/auth.routes.js
@@ -1,15 +1,21 @@
 import express from "express";
-import { forgotUserPassword, loginUser, logoutUser, resetUserPassword, signupUser } from "../controllers/auth.controllers.js";
+import { forgotUserPassword, getMyProfile, loginUser, logoutUser, resetUserPassword, signupUser } from "../controllers/auth.controllers.js";
 import authenticateUser from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+// Public routes No authentication required
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
-
-router.post("/logout", authenticateUser, logoutUser);
-
 router.post("/forgot-password", forgotUserPassword);
 router.post("/reset-password", resetUserPassword);
+
+// Authentication middleware applied to all routes below
+router.use(authenticateUser);
+
+
+// Protected routes
+router.post("/logout", logoutUser);
+router.get("/profile", getMyProfile);
 
 export default router;
