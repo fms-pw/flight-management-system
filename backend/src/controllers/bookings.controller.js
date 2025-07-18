b671080cc015b5fd4eab4d52f39a8481f33736fe
Kailash Parihar <kailashparihar1999@gmail.com>
2025-07-18 10:47:36 +0530

    Rename booking.controller.js to bookings.controller.js and added bookings route handler

---
+++ backend/src/controllers/bookings.controller.js
@@ -0,0 +1,17 @@
+export const createBooking = () => {
+  // Ticket Booking Handler
+};
+
+export const getMyBookings = () => {
+  // View own booked ticked  handler
+};
+export const cancelBooking = () => {
+  // View own booked ticked  handler
+};
+export const rescheduleBooking = () => {
+  // View own booked ticked  handler
+};
+
+export const getAllBookings = () => {
+  // View all bookings handler only for admin
+};
