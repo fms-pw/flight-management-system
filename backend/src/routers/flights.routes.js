9f5da8cbfea53a0bc6b29660324e01003650aac5
Kailash Parihar <kailashparihar1999@gmail.com>
2025-07-18 11:09:48 +0530

    Remove flights_status.routes.js and merge status routes into flights.routes.js

--- backend/src/routers/flights.routes.js
+++ backend/src/routers/flights.routes.js
@@ -1,16 +1,32 @@
 import express from "express";
+import authenticateUser from "../middlewares/auth.middleware.js";
+import authorizeUserRoles from "../middlewares/authorizeRole.middleware.js";
 import {
+  createFlight,
+  deleteFlight,
   getAllFlights,
   getFlightById,
-  createFlight,
-  updateFlight,
-  deleteFlight,
-} from "../controllers/flights.controllers.js";
+  updateFlightById,
+} from "../controllers/flights.controller.js";
 
-export let flightsRouter=express.Router();
-flightsRouter.post("/createFlight",createFlight);
-flightsRouter.patch("/updateFlight/:id",updateFlight);
-flightsRouter.delete("/deleteFlight/:id",deleteFlight);
-flightsRouter.get("getAFlight/:id",getFlightById);
-flightsRouter.get("getAllFlights",getAllFlights); //admin middleware must be added here
+import { getFlightStatusByCode, updateFlightStatus } from "../controllers/flightStatus.controller.js";
 
+const router = express.Router();
+
+// Authentication applied to all routes below
+router.use(authenticateUser);
+
+router.get("/status/:code", getFlightStatusByCode);
+
+// Authorization applied to all routes below
+router.use(authorizeUserRoles(["admin"]));
+
+router.put("/:id/status", updateFlightStatus);
+
+router.post("/", createFlight);
+router.get("/", getAllFlights);
+router.get("/:id", getFlightById);
+router.put("/:id", updateFlightById);
+router.delete("/:id", deleteFlight);
+
+export default router;
--- backend/src/routers/flights_status.routes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getFlightStatusByCode, updateFlightStatus } from "../controllers/flights_status.controller.js";
-
-
-export const fli_statusRouter = express.Router();
-
-fli_statusRouter.put("/:id/status", updateFlightStatus);
-fli_statusRouter.get("/status/:code", getFlightStatusByCode);
