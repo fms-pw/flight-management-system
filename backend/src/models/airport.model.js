4594910785026960b4cf8c4acd4b6168e04ce85c
Kailash Parihar <kailashparihar1999@gmail.com>
2025-07-20 04:21:41 +0530

    Update Airport schema: added IATA/ICAO code length, terminal field, and trimming for name

--- backend/src/models/airport.model.js
+++ backend/src/models/airport.model.js
@@ -1,10 +1,21 @@
-import mongoose from 'mongoose';
-const airportSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true, unique: true }, //for e.g., DEL, BOM
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-}, { timestamps: true });
+import mongoose from "../database/mongodb.config.js";
 
- const Airport = mongoose.model('Airport', airportSchema);
- export default Airport;
+const airportSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+
+    iataCode: { type: String, required: true, unique: true, uppercase: true, length: 3 },
+    icaoCode: { type: String, required: false, unique: true, uppercase: true, length: 4 },
+
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    terminals: { type: Number },
+  },
+  {
+    timestamps: true,
+    minimize: false,
+  }
+);
+
+const Airport = mongoose.model("Airport", airportSchema);
+export default Airport;
